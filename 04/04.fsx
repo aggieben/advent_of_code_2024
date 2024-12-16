170f717622f877617f838abf0f930f5aab5cdb9b
Ben Collins <=>
2024-12-16 12:59:06 -0600

    day 4 part 2

--- 04/04.fsx
+++ 04/04.fsx
@@ -45,4 +45,41 @@ let findWords (bytes:byte[][]) =
     |> Seq.sumBy (fun (row, col) -> findWordsFrom bytes row col)
     // |> Seq.iter (fun c -> printfn "%A" c)
 
-findWords input
+// findWords input
+
+(* part two *)
+
+let M = word[1]
+let A = word[2]
+let S = word[3]
+let vecnw = (-1, -1)
+let vecne = (-1, 1)
+let vecsw = (1, -1)
+let vecse = (1, 1)
+
+let isXOnPoint (bytes:byte[][]) coord =
+    let nw = coord + vecnw
+    let ne = coord + vecne
+    let sw = coord + vecsw
+    let se = coord + vecse
+
+    if  not (coordValid bytes coord)
+     || not (coordValid bytes nw)
+     || not (coordValid bytes ne)
+     || not (coordValid bytes sw)
+     || not (coordValid bytes se)
+    then false
+    else
+        let result =
+            (bytes[fst coord][snd coord] = A) &&
+            ((bytes[fst nw][snd nw] = M && bytes[fst se][snd se] = S) || (bytes[fst nw][snd nw] = S && bytes[fst se][snd se] = M)) &&
+            ((bytes[fst sw][snd sw] = M && bytes[fst ne][snd ne] = S) || (bytes[fst sw][snd sw] = S && bytes[fst ne][snd ne] = M))
+        printfn "isXOnPoint %A (%A) [nw: %A ne: %A sw: %A se: %A]: %A" coord (bytes[fst coord][snd coord]) nw ne sw se result
+        result
+
+let findXs (bytes:byte[][]) =
+    iterate2d (Array.length bytes) (Array.length bytes[0])
+    |> Seq.sumBy (fun coord -> if isXOnPoint bytes coord then 1 else 0)
+    // |> Seq.iter (fun c -> printfn "%A" c)
+
+findXs input
