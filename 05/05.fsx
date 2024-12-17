0b694894c9ae2a64e3ae8e96d9f21e5552ebaa0b
Ben Collins <aggieben@gmail.com>
2024-12-16 19:33:44 -0600

    day 5a complete

--- 05/05.fsx
+++ 05/05.fsx
@@ -2,11 +2,9 @@ open System
 open System.IO
 open System.Text
 
-let inputFile = "05/05_test.txt"
+let inputFile = "05/05_input.txt"
 
 let input = File.ReadLines inputFile
-
-// fun line -> line.Split("|") |> (fun arr -> int arr[0], int arr[1])
 
 let parsedInput =
     ({| inRules = true; rules = []; updates = []|}, input)
@@ -29,4 +27,25 @@ let insertMapList key value map =
     | None -> Map.add key [value] map
 
 let ruleMap = parsedInput.rules |> List.fold (fun map (a, b) -> insertMapList a b map) Map.empty
+
+let updateIsValid update =
+    let updateMap = update |> Array.mapi (fun i v -> (v, i)) |> Map.ofArray
+
+    // the semantic here is "are all pages in the update well-ordered?"
+    update |>
+        Array.forall (
+            fun page ->
+                match Map.tryFind page ruleMap with
+                | Some subsequents ->
+                    subsequents |> List.forall (fun sub ->
+                                                    match Map.tryFind sub updateMap with
+                                                    | Some subIndex -> subIndex > updateMap[page]
+                                                    | None -> true)
+                | None -> true)
+
+let validUpdates =
+    updates
+    |> List.filter updateIsValid
+
+validUpdates |> List.sumBy (fun update -> update[(update.Length - 1)/2])
 
